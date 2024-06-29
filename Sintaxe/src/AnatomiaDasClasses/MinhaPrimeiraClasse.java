0c9b792030d62cd243d2fdda113d011ed205add4
Marcia P. Moreno <mapat.soad@gmail.com>
2024-06-29 12:33:02 -0300

    Inserindo Minha Primeira Classe Ola mundo

---
+++ Sintaxe/src/AnatomiaDasClasses/MinhaPrimeiraClasse.java
@@ -0,0 +1,8 @@
+package AnatomiaDasClasses;
+
+public class MinhaPrimeiraClasse {
+
+    public static void main(String[] args) {
+        System.out.println("Olá mundo! Esta é minha primeira classe.java...");
+    }
+}
