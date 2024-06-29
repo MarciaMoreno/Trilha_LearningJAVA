a14a0921feea8a5d437cfc196c21ec2e01cd62cc
Marcia P. Moreno <mapat.soad@gmail.com>
2024-06-29 12:40:30 -0300

    Minha classe Nome de Variáveis

---
+++ Sintaxe/src/AnatomiaDasClasses/NomeDeVariavel.java
@@ -0,0 +1,16 @@
+package AnatomiaDasClasses;
+
+public class NomeDeVariavel {
+
+    public static void main(String[] args) {
+        String BR = "Brasil";
+        double PI = 3.14;
+        int ESTADOS_BRASILEIRO = 27;
+        int ANO = 2000;
+
+        System.out.println("País: "+BR);
+        System.out.println("Índice Populacional: "+PI+" bilhões");
+        System.out.println("Quantidade de Estados: "+ESTADOS_BRASILEIRO);
+        System.out.println("Ano Base: "+ANO);
+    }
+}
