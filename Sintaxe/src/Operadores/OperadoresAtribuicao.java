5fc7fc231c57c51d5e0fdd08f15ef9f637f887f3
Marcia P. Moreno <mapat.soad@gmail.com>
2024-06-29 18:25:27 -0300

    Inserindo exemplo de Operadores de Atribuição

--- Sintaxe/src/Operadores/OperadoresAtribuicao.java
+++ Sintaxe/src/Operadores/OperadoresAtribuicao.java
@@ -15,7 +15,13 @@ public class OperadoresAtribuicao {
         boolean doadorOrgao = false;
         String dataNascimento = "02041985081300";
 
-        Date dt = new SimpleDateFormat("ddMMyyyy").parse(dataNascimento);
+        System.out.println("Nome Completo: "+nome);
+        System.out.println("Idade: "+idade+" anos");
+        System.out.println("Peso Atual: "+peso+" Kg");
+        System.out.println("Genero: "+genero);
+        if(doadorOrgao == true) System.out.println("Doador de Orgãos?: SIM.");
+        else System.out.println("Doador de Orgãos?: NÃO.");
+        Date dt = new SimpleDateFormat("ddMMyyyyHHmmss").parse(dataNascimento);
         System.out.print("Data de Nascimento: ");
         System.out.print(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(dt));
 
