d82a2f91659b25ceb8dcf4d2958b7dbe686603d6
Marcia P. Moreno <mapat.soad@gmail.com>
2024-06-29 13:33:58 -0300

    Exemplo de estrutura de uma variável

--- Sintaxe/src/TiposEVariaveis/ExemploEstrutura.java
+++ Sintaxe/src/TiposEVariaveis/ExemploEstrutura.java
@@ -1,5 +1,17 @@
-package AnatomiaDasClasses.TiposEVariaveis;
-
+package TiposEVariaveis;
 public class ExemploEstrutura {
 
+    public static void main(String[] args) {
+        System.out.println("Segue a estrutura básica: ");
+        System.out.println("<Tipo> <nomeVariavel> = <atribuicaoDeValor>"); 
+        
+        System.out.println("Exemplos abaixo...");
+        int idade = 0; //Tipo "int", nome "idade", com nenhum valor atribuído. 
+        int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com valor 2021.
+        double valorMinimo = 2.500; //tipo "double", nome "salarioMinimo", valor 2.500.
+
+        System.out.println("int idade = 0; resultado deste tipo é : "+idade);
+        System.out.println("int anoFabricacao = 2021; resultado deste tipo é : "+anoFabricacao);
+        System.out.println("double valorMinimo = 2.500; resultado deste tipo é : "+valorMinimo);
+    }
 }
