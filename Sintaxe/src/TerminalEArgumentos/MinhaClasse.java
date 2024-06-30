d555a9eb6030fdf23c5fd52125cc8a9c7f829a58
Marcia P. Moreno <mapat.soad@gmail.com>
2024-06-29 22:54:30 -0300

    Teste de Terminal e composição de arrays

---
+++ Sintaxe/src/TerminalEArgumentos/MinhaClasse.java
@@ -0,0 +1,8 @@
+package TerminalEArgumentos;
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        System.out.println("Ola mundo! Estou sendo executado pelo terminal...");
+    }
+}
