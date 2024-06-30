2c0bc9ad19ff6c3ead15ec9e7ecbe92296344102
Marcia P. Moreno <mapat.soad@gmail.com>
2024-06-30 00:28:06 -0300

    Inserção de Classe para exemplo de estrutura de condicional Simples

---
+++ ControleDeFluxo/src/EstruturasCondicionais/CaixaEletronico.java
@@ -0,0 +1,15 @@
+package EstruturasCondicionais;
+
+public class CaixaEletronico {
+
+    public static void main(String[] args) {
+        //Exemplo de uso para uma Condicional Simples...
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+        System.out.println("Saldo disponivel antes do saque: R$"+saldo);
+        System.out.println("Valor solicitado para saque: R$"+valorSolicitado);
+        if(valorSolicitado < saldo)
+        saldo = saldo - valorSolicitado; 
+        System.out.println("Disponivel para novo saque: R$"+saldo);
+    }
+}
