5444ac846c034d67a0f18cb7db3dabd615839686
Marcia P. Moreno <mapat.soad@gmail.com>
2024-06-30 11:58:32 -0300

    Implementando import scanner nos scripts de exemplo

--- ControleDeFluxo/src/EstruturasDeCondicao/ExemploDeSwitch.java
+++ ControleDeFluxo/src/EstruturasDeCondicao/ExemploDeSwitch.java
@@ -1,38 +1,48 @@
 package EstruturasDeCondicao;
 
+import java.util.Scanner;
+
 public class ExemploDeSwitch {
      
     public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        System.out.println("UTILIZANDO UMA CONDIÇÃO DE LOOP IF/ELSE...");
-        String sigla = "M";
-		if(sigla == "P")
+        System.out.println("UTILIZANDO UMA CONDIÇÃO DE LOOP IF/ELSE...");		
+		System.out.println("Escolha um tamanho");
+		System.out.println("M - MEDIO");
+		System.out.println("P - PEQUENO");
+		System.out.println("G - GRANDE");
+        String sigla = scanner.next();
+		if(sigla == "P" && sigla == "p")
 			System.out.println("PEQUENO");
-		else if(sigla == "M")
+		else if(sigla == "M" && sigla == "m")
 			System.out.println("MÉDIO");
-		else if(sigla == "G")
+		else if(sigla == "G" && sigla == "g")
 			System.out.println("GRANDE");
 		else
-			System.out.println("INDEFINIDO");
+			System.out.println("Não corresponde nenhuma das opções...");
 
         System.out.println("\n\nUTILIZANDO O MESMO CÓDIGO COM SWITCH...");
-        String sigla2 = "M";
-
+		System.out.println("Escolha um tamanho");
+		System.out.println("M - MEDIO");
+		System.out.println("P - PEQUENO");
+		System.out.println("G - GRANDE");
+        String sigla2 = scanner.next();
 		switch (sigla2) {
-		case "P":{
+		case "P","p":{
 			System.out.println("PEQUENO");
 			break;
 		}
-		case "M":{
+		case "M","m":{
 			System.out.println("MÉDIO");
 			break;
 		}
-		case "G":{
+		case "G","g":{
 			System.out.println("GRANDE");
 			break;
 		}
 		default:
-			System.out.println("INDEFINIDO");
+			System.out.println("Não corresponde nenhuma das opções...");
 		}
     }
 }
---
+++ ControleDeFluxo/src/EstruturasDeCondicao/PlanoOperadora.java
@@ -0,0 +1,19 @@
+package EstruturasDeCondicao;
+
+public class PlanoOperadora {
+
+    public static void main(String[] args) {
+        String plano = "M"; //B / T
+		
+		if(plano == "B") {
+			System.out.println("100 minutos de ligação");
+		}else if(plano == "M") {
+			System.out.println("100 minutos de ligação");
+			System.out.println("WhatsApp e Instagram grátis");	
+		}else if(plano == "T") {
+			System.out.println("100 minutos de ligação");
+			System.out.println("WhatsApp e Instagram grátis");	
+			System.out.println("5Gb Youtube");	
+		}
+    }
+}
