aed51e51953f246b4049ae1afd44461bd0f0d358
Marcia P. Moreno <mapat.soad@gmail.com>
2024-07-01 13:10:19 -0300

    Alteração do escopo da classe Escola e estudante

--- ProgramacaoOrientadaAObjetos/src/TiposDeClasses/Escola.java
+++ ProgramacaoOrientadaAObjetos/src/TiposDeClasses/Escola.java
@@ -3,29 +3,9 @@ package TiposDeClasses;
 public class Escola {
 
     public static void main(String[] args) {
-        Estudante estudante = new Estudante();
-        estudante.name = "Carlos Drummond Pesada";
-        estudante.idade = 9;
-        estudante.serie = "3º B";
-        estudante.genero = "M";
-
-        Estudante estudante1 = new Estudante();
-        estudante.name = "Cesani Carla Fiorri";
-        estudante.idade = 9;
-        estudante.serie = "3º B";
-        estudante.genero = "F";
-
-        Estudante estudante2 = new Estudante();
-        estudante.name = "Daiane Mitsuri";
-        estudante.idade = 9;
-        estudante.serie = "3º B";
-        estudante.genero = "F";
-
-        Estudante estudante3 = new Estudante();
-        estudante.name = "Antonio Sergio da Cruz";
-        estudante.idade = 9;
-        estudante.serie = "3º B";
-        estudante.genero = "M";
+       
+    
+    
     }
 
 }
--- ProgramacaoOrientadaAObjetos/src/TiposDeClasses/Estudante.java
+++ ProgramacaoOrientadaAObjetos/src/TiposDeClasses/Estudante.java
@@ -2,19 +2,6 @@ package TiposDeClasses;
 
 public class Estudante {
 
-    String name;
-    int idade;
-    String serie;
-    String genero;
-
-    public void comer(){
-        System.out.println("O aluno"+name+"está em horário de almoço...");
-    }
-    public void beber(){
-        System.out.println("O nosso aluno esta bebendo água...");
-    }
-    public void correr(){
-        System.out.println("O nosso aluno esta correndo...");
-    }
+    
 
 }
