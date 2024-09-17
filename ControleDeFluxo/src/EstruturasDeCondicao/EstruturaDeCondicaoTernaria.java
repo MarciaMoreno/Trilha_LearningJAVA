ad9dc9f12af216beaf31f530f72f37af4b2e202a
Marcia P. Moreno <mapat.soad@gmail.com>
2024-09-17 11:16:25 -0300

    Update EstruturaDeCondicaoTernaria.java

--- ControleDeFluxo/src/EstruturasDeCondicao/EstruturaDeCondicaoTernaria.java
+++ ControleDeFluxo/src/EstruturasDeCondicao/EstruturaDeCondicaoTernaria.java
@@ -4,13 +4,15 @@ public class EstruturaDeCondicaoTernaria {
 
     public static void main(String[] args) {
         System.out.println("Exemplo - Estrutura de condição Unária... ");
-        int nota = 7;System.out.println("Aluno com nota = "+nota);
-		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
-		System.out.println(resultado);
+        int nota = 7;
+	    System.out.println("Aluno com nota = "+nota);
+	    String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+	    System.out.println(resultado);
 
         System.out.println("Exemplo - Estrutura de condição Ternária... ");
-        int nota2 = 6;System.out.println("Aluno com nota = "+nota2);
-		String resultado2 = nota2 >=7 ? "Aprovado" : nota2 >=5 && nota2 <7 ? "Recuperação" : "Reprovado";
-		System.out.println(resultado2);
+        int nota2 = 6;
+	    System.out.println("Aluno com nota = "+nota2);
+	    String resultado2 = nota2 >=7 ? "Aprovado" : nota2 >=5 && nota2 <7 ? "Recuperação" : "Reprovado";
+	    System.out.println(resultado2);
     }
 }
